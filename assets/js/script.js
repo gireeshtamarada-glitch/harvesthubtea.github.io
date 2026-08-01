347d1fcb25ac8b8cc03004d9a03a7d63529c7714
harvesthubtea <gireeshtamarada@gmail.com>
2026-08-01 12:52:43 +0530

    Fix invalid hash selector error

--- assets/js/script.js
+++ assets/js/script.js
@@ -329,20 +329,20 @@ document.head.appendChild(style);
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
 
-        const href = this.getAttribute('href');
+    const href = this.getAttribute('href');
 
-        if (!href || href === "#") {
-            return;
-        }
+    if (!href || href === "#") {
+        return;
+    }
 
-        const target = document.querySelector(href);
+    const target = document.querySelector(href);
 
-        if (target) {
-            e.preventDefault();
-            target.scrollIntoView({
-                behavior: "smooth"
-            });
-        }
+    if (target) {
+        e.preventDefault();
 
-    });
+        target.scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+
 });
