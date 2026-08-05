8d96bbfb22a695dea93fb5327499b30776b7b20f
harvesthubtea <gireeshtamarada@gmail.com>
2026-08-05 19:22:19 +0530

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -353,17 +353,15 @@ async function updateOrderStatus(index) {
 
     try {
 
-        const response = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                action: "updateStatus",
-                orderId: order.orderId,
-                status: newStatus
-            })
-        });
+       const response = await fetch(API_URL, {
+    method: "POST",
+    mode: "no-cors",
+    body: JSON.stringify({
+        action: "updateStatus",
+        orderId: order.orderId,
+        status: newStatus
+    })
+});
 
         const result = await response.json();
 
